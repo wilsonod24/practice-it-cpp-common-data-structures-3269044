39b8b2299f6d554b5598f6d1244ad0763b6f0d42
wilsonod24 <149526803+wilsonod24@users.noreply.github.com>
2024-09-14 21:48:31 +0000

    Pending changes exported from your codespace

--- src/Ch01-Arrays/01_02/end/sum-array.cpp
+++ src/Ch01-Arrays/01_02/end/sum-array.cpp
@@ -3,7 +3,11 @@ using namespace std;
 
 int calculateSum(const int arr[], size_t size)
 {
-    //...TODO: implement
+    int sum = 0;
+    for (int i = 0; i < size; i++){
+        sum += arr[i];
+    }
+    return sum;
 }
 
 int main()
